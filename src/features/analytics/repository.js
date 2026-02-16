6efb412e4cacc053869e0dec56b47ab7303391bb
Shiminize <kyle.x.lam@gmail.com>
2026-02-17 02:31:45 +0800

    Fix: Enable full person tracking in PostHog to troubleshoot missing data

--- src/features/analytics/repository.js
+++ src/features/analytics/repository.js
@@ -12,7 +12,7 @@ const AnalyticsRepository = (function () {
         try {
             posthog.init(POSTHOG_API_KEY, {
                 api_host: POSTHOG_HOST,
-                person_profiles: 'identified_only' // Optimization for anon users
+                person_profiles: 'always' // Ensure all users are tracked
             });
         } catch (e) {
             console.warn("Analytics: PostHog init failed", e);
@@ -22,7 +22,10 @@ const AnalyticsRepository = (function () {
         !function (t, e) { var o, n, p, r; e.__SV || (window.posthog = e, e._i = [], e.init = function (i, s, a) { function g(t, e) { var o = e.split("."); 2 == o.length && (t = t[o[0]], e = o[1]), t[e] = function () { t.push([e].concat(Array.prototype.slice.call(arguments, 0))) } } (p = t.createElement("script")).type = "text/javascript", p.async = !0, p.src = s.api_host + "/static/array.js", (r = t.getElementsByTagName("script")[0]).parentNode.insertBefore(p, r); var u = e; for (void 0 !== a ? u = e[a] = [] : a = "posthog", u.people = u.people || [], u.toString = function (t) { var e = "posthog"; return "posthog" !== a && (e += "." + a), t || (e += " (stub)"), e }, u.people.toString = function () { return u.toString(1) + ".people (stub)" }, o = "capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys getNextSurveyStep".split(" "), n = 0; n < o.length; n++)g(u, o[n]); e._i.push([i, s, a]) }, e.__SV = 1) }(document, window.posthog || []);
 
         try {
-            posthog.init(POSTHOG_API_KEY, { api_host: POSTHOG_HOST });
+            posthog.init(POSTHOG_API_KEY, {
+                api_host: POSTHOG_HOST,
+                person_profiles: 'always' // Ensure all users are tracked
+            });
         } catch (e) { console.warn("Analytics: PostHog lazy load init failed", e); }
     }
 
